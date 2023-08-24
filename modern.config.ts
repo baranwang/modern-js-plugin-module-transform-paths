31aa62fd8f78094a38b593d3217ccf1be2a659c3
baran.wang <baran.wang@bytedance.com>
2023-08-24 10:32:23 +0800

    feat: init

--- modern.config.ts
+++ modern.config.ts
@@ -2,5 +2,11 @@ import { moduleTools, defineConfig } from '@modern-js/module-tools';
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildPreset: 'npm-library',
+  buildConfig: {
+    buildType: 'bundleless',
+    format: 'cjs',
+    target: 'es2019',
+    outDir: './dist',
+    externalHelpers: true,
+  },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export default function () {
-  return 'hello world';
-}
+import { CliPlugin, ModuleTools } from '@modern-js/module-tools';
+
+export const modulePluginTransformPaths = (): CliPlugin<ModuleTools> => {
+  return {
+    name: 'transform-paths-plugin',
+    setup() {
+      return {
+        beforeBuild(options) {
+          console.log(options);
+        },
+      };
+    },
+  };
+};
